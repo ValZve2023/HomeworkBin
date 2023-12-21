cb0de2857effb08e3ef8921bcd96447fbd0af325
ValZve2023 <frau.zvereva2009@yandex.ru>
2023-12-21 18:57:55 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/HomeworkBin/HomeworkBinApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.HomeworkBin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/HomeworkBin/HomeworkBinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.HomeworkBin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
